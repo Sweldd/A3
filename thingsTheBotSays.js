67393db542553c4cb7b01549d8a608245f5419f4
Francis Dharmasiri <sweldd2g5@gmail.com>
2019-10-22 19:42:40 +0800

    edit

---
+++ thingsTheBotSays.js
@@ -0,0 +1,15 @@
+/*
+This JavaScript file has the robot's 'script', which is the list of questions or statements the robot will send as chat.
+*/
+
+
+// botScript is a list of questions the robot will ask the user.
+var botScript = [
+  "Hello! What is your name?",
+  "This is the second thing I say",
+  "This is the third thing I say",
+  "This is the last thing I say before I say goodbye"
+];
+
+// goodbye is a variable that stores what the robot will say when it runs out of other things to say.
+var goodbye = "Goodbye! I have to go now.";
