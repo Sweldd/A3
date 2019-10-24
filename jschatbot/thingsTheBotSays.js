bfcb29aed57afa9b52c99da8f472952290446f13
Jordan Draganoff <jordan.draganoff@gmail.com>
2019-10-24 16:31:35 +1000

    added chatbot

---
+++ jschatbot/thingsTheBotSays.js
@@ -0,0 +1,15 @@
+/*
+This JavaScript file has the robot's 'script', which is the list of questions or statements the robot will send as chat.
+*/
+
+
+// botScript is a list of questions the robot will ask the user.
+var botScript = [
+  "Please enter your name",
+  "What helped the last time you were depressed?",
+  "Please tell me the name of your most trusted colleague"
+];
+
+
+// goodbye is a variable that stores what the robot will say when it runs out of other things to say.
+var goodbye = "According to your answers";
